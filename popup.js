18c04193f340e033d847d6d800c8e3a1d7bd0a36
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2024-12-29 19:24:47 +0700

    feat: handle logic state button

--- popup.js
+++ popup.js
@@ -1,23 +1,55 @@
-const playBtn = document.getElementById("play")
-const pauseBtn = document.getElementById("pause")
+const playBtn = document.getElementById("play");
+const pauseBtn = document.getElementById("pause");
+
+const listOfActions = [
+    {
+        name: "play",
+        action: () => {
+            playBtn.disabled = true;
+            pauseBtn.disabled = false;
+            saveButtonState("play");
+        },
+    },
+    {
+        name: "pause",
+        action: () => {
+            playBtn.disabled = false;
+            pauseBtn.disabled = true;
+            saveButtonState("pause");
+        },
+    },
+];
 
 function init() {
     playBtn.disabled = false;
     pauseBtn.disabled = true;
 
-    chrome.runtime.sendMessage({ action: 'createPlayer' });
+     chrome.storage.local.get("buttonState", (result) => {
+        const state = result.buttonState || "pause";
+        handleButtonState(state);
+    });
 
     playBtn.addEventListener("click", () => {
-        chrome.runtime.sendMessage({ action: 'play' });
-        playBtn.disabled = true;
-        pauseBtn.disabled = false;
+        chrome.runtime.sendMessage({ action: "play" });
+        handleButtonState("play");
     });
 
     pauseBtn.addEventListener("click", () => {
-        chrome.runtime.sendMessage({ action: 'pause' });
-        playBtn.disabled = false;
-        pauseBtn.disabled = true;
+        chrome.runtime.sendMessage({ action: "pause" });
+        handleButtonState("pause");
     });
+}
+
+function handleButtonState(action) {
+    for (const item of listOfActions) {
+        if (item.name === action) {
+            item.action();
+        }
+    }
+}
+
+function saveButtonState(state) {
+    chrome.storage.local.set({ buttonState: state });
 }
 
 init();
