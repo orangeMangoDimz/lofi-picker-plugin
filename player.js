adf76299f5d0653aa06af64380f6fb0b2ae6eb9c
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2024-12-30 15:56:46 +0700

    ref(song): refactor play and pause msuic logic

--- player.js
+++ player.js
@@ -1,14 +1,33 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const iframe = document.getElementById("youtube-player");
 
-    if (message.action === "play" || message.action === "pause") {
-        iframe.contentWindow.postMessage(
-            JSON.stringify({
-                event: "command",
-                func: message.action + "Video",
-                args: [],
-            }),
-            "*",
-        );
+    const { action, data } = message;
+    const { videoId } = data;
+    if (!isTheSameVideo(iframe, videoId)) {
+        const url = `https://www.youtube.com/embed/${videoId}?enablejsapi=1`;
+        iframe.src = url;
+        iframe.onload = () => {
+            if (action === "pause" || action === "play") {
+                changeMusic(iframe, action);
+            }
+        };
+    } else {
+        changeMusic(iframe, action);
     }
 });
+
+function isTheSameVideo(iframe, videoId) {
+    const currentVideoId = iframe.src.split("/").pop().split("?")[0];
+    return currentVideoId === videoId;
+}
+
+function changeMusic(iframe, action) {
+    iframe.contentWindow.postMessage(
+        JSON.stringify({
+            event: "command",
+            func: action + "Video",
+            args: [],
+        }),
+        "*",
+    );
+}
--- popup.js
+++ popup.js
@@ -1,57 +1,331 @@
-const playBtn = document.getElementById("play");
-const pauseBtn = document.getElementById("pause");
+const videoList = [
+    {
+        title: "for when it’s warmer",
+        videoId: "hG6YzmvbosI",
+        videoOwnerChannelTitle: "sleepy fish - Topic",
+        videoOwnerChannelId: "UCNR1Pzl2T9biZWAbOXZPUwQ",
+    },
+    {
+        title: "Lofi Hip Hop | Leaves",
+        videoId: "F1tAoCuof6c",
+        videoOwnerChannelTitle: "TSN Vibes",
+        videoOwnerChannelId: "UC1Z4DDUyXeGFSIL7FR-vFQg",
+    },
+    {
+        title: "Chilling With Lofi",
+        videoId: "M6zDqbdYLYo",
+        videoOwnerChannelTitle: "Sad Music - Topic",
+        videoOwnerChannelId: "UC9snFmgnSARriqBNhk4D_cw",
+    },
+    {
+        title: "Peaceful Sleep",
+        videoId: "XPpYWLl-O10",
+        videoOwnerChannelTitle: "No Spirit - Topic",
+        videoOwnerChannelId: "UCraRksyJpUyJQexB7-hdoTQ",
+    },
+    {
+        title: "2021 Is All Jazz",
+        videoId: "fHXaNXA_NKc",
+        videoOwnerChannelTitle: "Lofi Sleep Chill & Study - Topic",
+        videoOwnerChannelId: "UC1itStv5HQODNJr1zyUmDyg",
+    },
+    {
+        title: "Blankets",
+        videoId: "HdXrkgZP438",
+        videoOwnerChannelTitle: "Aso - Topic",
+        videoOwnerChannelId: "UCgWzwhihlRaf6t4Bu8cRh6w",
+    },
+    {
+        title: "When I See You",
+        videoId: "xbxW2_icZjM",
+        videoOwnerChannelTitle: "Kudasaibeats - Topic",
+        videoOwnerChannelId: "UCumJMf6sc_oiLpIElQWUiOQ",
+    },
+    {
+        title: "Birds",
+        videoId: "xoopso_csJE",
+        videoOwnerChannelTitle: "Allem Iversom - Topic",
+        videoOwnerChannelId: "UC7oHoI9Xs-QAR81y6TjikyA",
+    },
+    {
+        title: "Rainforest",
+        videoId: "Arx6UcDo0T0",
+        videoOwnerChannelTitle: "Saib - Topic",
+        videoOwnerChannelId: "UCw02oGFeqYXl14LcyKhOTYw",
+    },
+    {
+        title: "Silience",
+        videoId: "cyKX1uZgtt8",
+        videoOwnerChannelTitle: "Toonorth - Topic",
+        videoOwnerChannelId: "UCF_6iylCppB8bwhnW3HmcRw",
+    },
+    {
+        title: "Move Together",
+        videoId: "atOeCrsxNoE",
+        videoOwnerChannelTitle: "Yasper - Topic",
+        videoOwnerChannelId: "UCo0JVM02GNDSKXAoE4JrUYQ",
+    },
+    {
+        title: "Slow Drive",
+        videoId: "wy1R4zcBNZ4",
+        videoOwnerChannelTitle: "ChillHop - Topic",
+        videoOwnerChannelId: "UCvuWT6OEzQga3mNlgInpYnQ",
+    },
+    {
+        title: "Meaux",
+        videoId: "aPHPbb2kFz4",
+        videoOwnerChannelTitle: "Ian Ewing - Topic",
+        videoOwnerChannelId: "UC93IJn52Fc5tVWcSA3GHbzg",
+    },
+    {
+        title: "Soulful",
+        videoId: "fs0BCIUQ7Ws",
+        videoOwnerChannelTitle: "L'Indécis - Topic",
+        videoOwnerChannelId: "UCxXn2yb4qJbG0bKmr3wa4JA",
+    },
+    {
+        title: "Daybreak",
+        videoId: "y3PKcVd7UtM",
+        videoOwnerChannelTitle: "cocabona - Topic",
+        videoOwnerChannelId: "UCGufCXbmC64ajXVhIaCTpqw",
+    },
+    {
+        title: "Velocities",
+        videoId: "JgI6z6aQhEA",
+        videoOwnerChannelTitle: "sleepy fish - Topic",
+        videoOwnerChannelId: "UCNR1Pzl2T9biZWAbOXZPUwQ",
+    },
+    {
+        title: "2020",
+        videoId: "xuXUkynqITU",
+        videoOwnerChannelTitle: "idealism - Topic",
+        videoOwnerChannelId: "UCOc-AHiKRo_Z8rSHPM74Wag",
+    },
+    {
+        title: "Your Favorite Place",
+        videoId: "xaVAPmR6JMg",
+        videoOwnerChannelTitle: "Joey Pecoraro - Topic",
+        videoOwnerChannelId: "UCT2-ZDqZq35BUz_gA7tYRWg",
+    },
+    {
+        title: "Forest Lake Walk",
+        videoId: "qGVMBirxwWc",
+        videoOwnerChannelTitle: "No Spirit - Topic",
+        videoOwnerChannelId: "UCraRksyJpUyJQexB7-hdoTQ",
+    },
+    {
+        title: "faces",
+        videoId: "S8p0crahq3A",
+        videoOwnerChannelTitle: "Austin Chen - Topic",
+        videoOwnerChannelId: "UCrsVHlnA_bA6kZ7rQW8ZnbA",
+    },
+    {
+        title: "Soul Food",
+        videoId: "XznpSTBVliM",
+        videoOwnerChannelTitle: "Ruck P - Topic",
+        videoOwnerChannelId: "UCdltCBFLglVYUCuyvMEk7BA",
+    },
+    {
+        title: "Hanging Lanterns",
+        videoId: "cukiBQ18NgE",
+        videoOwnerChannelTitle: "Kalaido - Topic",
+        videoOwnerChannelId: "UCnX3dJnTo1kf0zZuPcED_FA",
+    },
+    {
+        title: "Valley",
+        videoId: "yApGAVGvKDs",
+        videoOwnerChannelTitle: "Leavv   - Topic",
+        videoOwnerChannelId: "UC7l9fvTjR75Ul_Zexmb5z-w",
+    },
+    {
+        title: "Lofi Chill",
+        videoId: "h6TzXFARexQ",
+        videoOwnerChannelTitle: "Sad Music - Topic",
+        videoOwnerChannelId: "UC9snFmgnSARriqBNhk4D_cw",
+    },
+    {
+        title: "Jazzy Nights",
+        videoId: "Rq6Z-HzojV0",
+        videoOwnerChannelTitle: "Lofi Sleep Chill & Study - Topic",
+        videoOwnerChannelId: "UC1itStv5HQODNJr1zyUmDyg",
+    },
+    {
+        title: "Velvet Garden",
+        videoId: "ULgPNlTfinM",
+        videoOwnerChannelTitle: "City Girl - Topic",
+        videoOwnerChannelId: "UCWGqmBzWyhxn8a-YDSfb_LA",
+    },
+    {
+        title: "Whale Call",
+        videoId: "zIo1kXQuVz0",
+        videoOwnerChannelTitle: "Saib - Topic",
+        videoOwnerChannelId: "UCw02oGFeqYXl14LcyKhOTYw",
+    },
+    {
+        title: "Lunar Drive",
+        videoId: "WryLH9_cRkM",
+        videoOwnerChannelTitle: "Mondo Loops - Topic",
+        videoOwnerChannelId: "UCNFKlIyDJTdmV1oeHdSZ0Xg",
+    },
+    {
+        title: "Wistful",
+        videoId: "n9LXXyHjg3w",
+        videoOwnerChannelTitle: "Toonorth - Topic",
+        videoOwnerChannelId: "UCF_6iylCppB8bwhnW3HmcRw",
+    },
+    {
+        title: "Pastel Plains",
+        videoId: "Eh0ooPop5Ps",
+        videoOwnerChannelTitle: "fantompower - Topic",
+        videoOwnerChannelId: "UCVP1F5TF-aBurJkAdPMpW6g",
+    },
+];
 
 const listOfActions = [
     {
         name: "play",
-        action: () => {
+        action: (domElement, musicState) => {
+            const playBtn = domElement.getPlayBtn();
+            const pauseBtn = domElement.getPauseBtn();
             playBtn.disabled = true;
             pauseBtn.disabled = false;
-            playBtn.style.display = 'none';
-            pauseBtn.style.display = 'block';
-            saveButtonState("play");
+            playBtn.style.display = "none";
+            pauseBtn.style.display = "block";
+            const state = {
+                buttonState: "play",
+                musicState: musicState,
+            };
+            saveButtonState(state);
         },
     },
     {
         name: "pause",
-        action: () => {
+        action: (domElement, musicState) => {
+            const playBtn = domElement.getPlayBtn();
+            const pauseBtn = domElement.getPauseBtn();
             playBtn.disabled = false;
             pauseBtn.disabled = true;
-            playBtn.style.display = 'block';
-            pauseBtn.style.display = 'none';
-            saveButtonState("pause");
+            playBtn.style.display = "block";
+            pauseBtn.style.display = "none";
+            const state = {
+                buttonState: "play",
+                musicState: musicState,
+            };
+            saveButtonState(state);
         },
     },
 ];
 
-function init() {
-     chrome.storage.local.get("buttonState", (result) => {
-        const state = result.buttonState || "pause";
-        handleButtonState(state);
-    });
+function createDocument() {
+    const playBtn = document.getElementById("play");
+    const pauseBtn = document.getElementById("pause");
+    const songTitle = document.getElementById("song-title");
+    const songArtist = document.getElementById("song-artist");
 
-    playBtn.addEventListener("click", () => {
-        chrome.runtime.sendMessage({ action: "play" });
-        handleButtonState("play");
-    });
+    return {
+        getPlayBtn: () => {
+            return playBtn;
+        },
+        getPauseBtn: () => {
+            return pauseBtn;
+        },
+        getSongTitle: () => {
+            return songTitle;
+        },
+        getSongArtist: () => {
+            return songArtist;
+        },
+    };
+}
 
-    pauseBtn.addEventListener("click", () => {
-        chrome.runtime.sendMessage({ action: "pause" });
-        handleButtonState("pause");
-    });
+function createMusicState() {
+    let currMusicState = {
+        title: "Default Title",
+        videoId: "",
+        videoOwnerChannelTitle: "Default Channel",
+        videoOwnerChannelId: "",
+    };
+
+    return {
+        getMusicState: () => {
+            return currMusicState;
+        },
+        setMusicState: (musicState) => {
+            currMusicState = musicState;
+        },
+    };
+}
+
+function handleMusicLayout(domElement, musicState) {
+    const { title, videoOwnerChannelTitle } = musicState;
+    domElement.getSongTitle().textContent = title;
+    domElement.getSongArtist().textContent = videoOwnerChannelTitle;
 }
 
 function handleButtonState(action) {
+    const { buttonState, domElement, musicState } = action;
     for (const item of listOfActions) {
-        if (item.name === action) {
-            item.action();
+        if (item.name === buttonState) {
+            item.action(domElement, musicState);
             break;
         }
     }
 }
 
+function init() {
+    const currMusicState = createMusicState();
+    const domElement = createDocument();
+
+    chrome.storage.local.get(["buttonState", "musicState"], (result) => {
+        const { buttonState, musicState } = result;
+        const availableState = buttonState || "pause";
+
+        const randomIndexMusic = Math.floor(Math.random() * videoList.length);
+        const availableMusicState = musicState || videoList[randomIndexMusic];
+
+        currMusicState.setMusicState(availableMusicState);
+        handleMusicLayout(domElement, availableMusicState);
+        const sendState = {
+            domElement: domElement,
+            buttonState: availableState,
+            musicState: availableMusicState,
+        };
+        handleButtonState(sendState);
+    });
+
+    domElement.getPlayBtn().addEventListener("click", () => {
+        chrome.runtime.sendMessage({
+            action: "play",
+            data: currMusicState.getMusicState(),
+        });
+        const sendState = {
+            domElement: domElement,
+            buttonState: "play",
+            musicState: currMusicState.getMusicState(),
+        };
+        handleButtonState(sendState);
+    });
+
+    domElement.getPauseBtn().addEventListener("click", () => {
+        chrome.runtime.sendMessage({
+            action: "pause",
+            data: currMusicState.getMusicState(),
+        });
+        const sendState = {
+            domElement: domElement,
+            buttonState: "pause",
+            musicState: currMusicState.getMusicState(),
+        };
+        handleButtonState(sendState);
+    });
+}
+
 function saveButtonState(state) {
-    chrome.storage.local.set({ buttonState: state });
+    const { buttonState, musicState } = state;
+    chrome.storage.local.set({
+        buttonState: buttonState,
+        musicState: musicState,
+    });
 }
 
 init();
